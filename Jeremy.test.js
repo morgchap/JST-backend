836ce33dd408d74c0c31004d609ac52e216a6e07
jeremyleclere <leclere.jerem@gmail.com>
2024-12-16 09:15:08 +0100

    modifs weekend jerem avec un screen de plus

---
+++ Jeremy.test.js
@@ -0,0 +1,15 @@
+
+const request = require('supertest');
+const app = require('./app');
+
+it('DELETE /comments', async () => {
+ const res = await request(app).delete('/comments').send({
+    commentId: "ddzadzadazfgae83218321",
+
+})
+
+
+
+ expect(res.statusCode).toBe(200);
+ expect(res.body.stock).toBe(true);
+});
